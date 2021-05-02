d6202ac1d666f707619d30c0d4b4e0cb40d7c97f
Matt Macdonald Rasp4 <mksmacdonald@gmail.com>
2021-05-02 11:08:55 -0700

    created the ability to send poll to server

--- db/queries/db_put.js
+++ db/queries/db_put.js
@@ -6,7 +6,7 @@ const some_poll = {
   description: 'this is a description of our super awesome poll',
   admin_link: 'google.ca/figureitoutyourself',
   survey_link: 'lighthouselabs.ca',
-  time_created: Date.now(),
+  time_created: new Date(),
   time_closed: null,
   time_to_death: null
 }
@@ -23,30 +23,22 @@ const put_new_poll = function (poll) {
   const queryParams = [];
   // 2
   let queryString = `
-  INSERT INTO polls (creator_id, title, description, admin_link, survey_link, time_created, time_to_death)
-  VALUES(`;  
-  // VALUES($1, $2, $3, $4, $5, CURRENT_TIMESTAMP, 7$)`;  
+  INSERT INTO polls (creator_id, title, description, admin_link, survey_link, time_created, time_closed, time_to_death)
+  VALUES($1, $2, $3, $4, $5, $6, $7, $8)`;  
+  
   for(let key in poll) {
     queryParams.push(poll[key]);
-  } 
+  }  
 
-  for(let param of queryParams) {
-    if(typeof param === 'string'){
-      queryString += `'${param}', `;
-    } else {
-      queryString += `${param}, `;
-    }
-  }
-  queryString += `)`;
-
-  console.log('here is our querystring', queryString);
-  // console.log('here are the query params', queryParams);
-
-  return db_client.query(queryString);//.then((res) => res.rows);
+  return db_client.query(queryString, queryParams);//.then((res) => res.rows);
 };
 
 put_new_poll(some_poll)
-.then((result)=>{console.log(result)})
-.catch(console.log('HOLY FUCK WHAT THE HELL HAPPENED with creating a new pole'));
+.then((result)=>{console.log('We succesfully added a new table')})
+.catch(err => console.log('HOLY FUCK WHAT THE HELL HAPPENED with creating a new pole', err))
+.finally(()=>{
+  db_client.end();
+  console.log('i closed the db connection so we dont hang');
+});
 
 exports.put_new_poll = put_new_poll;
--- db/schema/02_polls.sql
+++ db/schema/02_polls.sql
@@ -1,6 +1,6 @@
 -- Please see AND UPDATE Notes/data_outline.md if changes are made here,  Thank you
 
--- DROP TABLE IF EXISTS polls CASCADE;
+DROP TABLE IF EXISTS polls CASCADE;
 CREATE TABLE polls (
   id SERIAL PRIMARY KEY,
   creator_id INTEGER,
@@ -14,7 +14,7 @@ CREATE TABLE polls (
 );
 
 -- the choices available for all polls - ideally used linked to the poll we want
-DROP TABLE IF EXISTS poll_choices CASCADE;
+DROP TABLE IF EXISTS poll_choices;
 CREATE TABLE poll_choices (
     id SERIAL PRIMARY KEY NOT NULL,         -- TODO MATT TODO MATT __ CHECK PROPER USAGE
     poll_id INTEGER REFERENCES polls(id), -- ON DELETE CASCADE,
@@ -22,7 +22,7 @@ CREATE TABLE poll_choices (
     rating INTEGER DEFAULT 0 -- Please see Notes/data_outline.md for current deffinition
 );
 
--- DROP TABLE IF EXISTS poll_unique_visits CASCADE;
+DROP TABLE IF EXISTS poll_unique_visits;
 CREATE TABLE poll_unique_visits (
     id SERIAL PRIMARY KEY NOT NULL, -- TODO MATT TODO MATT ___ CHECK USAGE
     poll_id INTEGER REFERENCES polls(id), -- ON DELETE CASCADE,
